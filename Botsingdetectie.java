2da127e1305364ed7ff513c0d5f45c7860e6f7dc
aareschluchtje <remcosannen@gmail.com>
2014-12-02 12:23:14 +0100

    Botsingdetectie aangepast voor voelsprieten

--- Botsingdetectie.java
+++ Botsingdetectie.java
@@ -2,29 +2,33 @@ package boebot;
 public class Botsingdetectie{
   private IrDetectie irL;
   private IrDetectie irR;
+  private VsDetectie vsL;
+  private VsDetectie vsR;
   private boolean IrDetectL;
   private boolean IrDetectR;
+  private boolean VsDetectL;
+  private boolean VsDetectR;
 
   public Botsingdetectie(){
        irL = new IrDetectie(8, 9);
        irR = new IrDetectie(2, 0);
+       vsL = new VsDetectie(8, 9);
+       vsR = new VsDetectie(2, 0);
   }
 
   public boolean detectEdge(){
 
     IrDetectL = irL.detect();
     IrDetectR = irR.detect();
-    System.out.println(IrDetectL);
-    System.out.println(IrDetectR);
-    if(IrDetectL || IrDetectR){
+    VsDetectL = vsL.detect();
+    VsDetectR = vsR.detect();
+
+    if(IrDetectL || IrDetectR || VsDetectL || VsDetectR){
         return true;
 
     }else{
       return false;
 
     }
-
-
-
   }
 }
