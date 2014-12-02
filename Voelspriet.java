4f5d67358dd55c57485fb07f01dd83a7695d792a
aareschluchtje <remcosannen@gmail.com>
2014-12-02 12:21:09 +0100

    Update Voelspriet.java

--- Voelspriet.java
+++ Voelspriet.java
@@ -1,6 +1,24 @@
 package boebot;
-public class Voelspriet{
- public Voelspriet(){
+import stamp.core.*;
 
+public class VsDetectie{
+PWM vsZender;
+  boolean vsDetect;
+  private int pinRead;
+  private int pinSend;
+
+ public VsDetectie(int pinS, int pinR){
+     vsZender = new PWM (CPU.pins[pinS], 1, 2);
+     vsZender.stop();
+     pinRead = pinR;
+     pinSend = pinS;
+
+ }
+ public void setPinRead(int pin){
+   pinRead = pin;
+ }
+ public boolean detect(){
+    vsDetect = CPU.readPin(CPU.pins[pinRead]);
+    return vsDetect;
  }
 }
