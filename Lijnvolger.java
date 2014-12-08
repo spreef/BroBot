ac4b148f2abd39d2db0770798e2c03a51264aa78
worms618 <worms618@users.noreply.github.com>
2014-12-08 11:09:08 +0100

    Update Lijnvolger.java
    De methode leest een sensor uit door de input pin en output pin in te stellen van je sensor.

--- Lijnvolger.java
+++ Lijnvolger.java
@@ -1,7 +1,21 @@
 package boebot; 
 public class Lijnvolger{
- public Lijnvolger(){
-
+ private int input;
+ private int output;
+ 
+ public Lijnvolger(int input, int output)){
+ this.input = input;
+ this.output = output;
+ }
+ 
+ public boolean detect(){
+ CPU.writePin(CPU.pins[input],true);           //zet spanning op QTI
+ CPU.delay(1);                                 //wacht even
+ qtiDigValue = CPU.readPin(CPU.pins[output]);  //lees QTI uit
+ CPU.writePin(CPU.pins[input],false);          //haal spanning van QTI
+ System.out.println(qtiDigValue);
+ CPU.delay(1000);
+ return qtiDigValue;
  }
 
 }
