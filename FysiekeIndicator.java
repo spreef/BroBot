50beb6fb2c21038b63f965614e026e8bdc8b8e03
Janco Kock <jancokock@gmail.com>
2014-12-09 15:27:04 +0100

    Update FysiekeIndicator.java

--- FysiekeIndicator.java
+++ FysiekeIndicator.java
@@ -7,7 +7,7 @@ public class FysiekeIndicator{
    public FysiekeIndicator(){
       led1 = new Led(1);
       led2 = new Led(10);
-      speaker = new Speaker(3);
+      speaker1 = new Speaker(3);
    }
    public void setLed1(boolean state){
      led1.setState(state);
