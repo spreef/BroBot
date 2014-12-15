a7c41a3c45c13578185c00268907f66e6d74daf0
worms618 <worms618@users.noreply.github.com>
2014-12-15 13:50:14 +0100

    Update Lijnvolger.java

--- Lijnvolger.java
+++ Lijnvolger.java
@@ -4,38 +4,34 @@ import stamp.core.*;
 public class Lijnvolger
 {
     boolean qtiDigValue;
-    IrLijnvolger IrL1;
-    IrLijnvolger IrL2;
-    IrLijnvolger IrL3;
+    IrLijnvolger IrMidden;
+    IrLijnvolger IrRechts;
+    IrLijnvolger IrLinks;
 
     public Lijnvolger()
     {
-      IrL1 = new IrLijnvolger(6, 5);
-      IrL2 = new IrLijnvolger(4, 3);
-      IrL3 = new IrLijnvolger(8, 7);
+      IrMidden = new IrLijnvolger(6, 5);
+      IrRechts = new IrLijnvolger(4, 3);
+      IrLinks = new IrLijnvolger(8, 7);
     }
 
 
     public int readSensor()
     {
       int waarde = -1;
-           if(IrL1.readSensorLijnvolger()){
-           //rijden(100,100,!false);
+          if(IrMidden.readSensorLijnvolger()){
            waarde = 0;
            }
 
-      else if(IrL2.readSensorLijnvolger()){
-           //rijden(20,100,!false);
-           waarde = 1;
+      else if(IrRechts.readSensorLijnvolger()){
+            waarde = 1;
            }
 
-      else if(IrL3.readSensorLijnvolger()){
-           //rijden(100,20,!false);
-           waarde = 2;
+      else if(IrLinks.readSensorLijnvolger()){
+            waarde = 2;
            }
       else{
-           //rijden(50,50,false);
-           waarde = 3;
+            waarde = 3;
            }
       return waarde;
     }
