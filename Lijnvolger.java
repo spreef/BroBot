7b156096799077c78754a8ba1d6e1003f9683682
Janco Kock <jancokock@gmail.com>
2015-01-06 12:12:07 +0100

    Nieuwe lijnvolger toegevoegd

--- Lijnvolger.java
+++ Lijnvolger.java
@@ -4,25 +4,27 @@ import stamp.core.*;
 public class Lijnvolger
 {
     boolean qtiDigValue;
-    IrLijnvolger IrMidden;
-    IrLijnvolger IrRechts;
-    IrLijnvolger IrLinks;
+    IrLijnvolger IrMidden, IrRechts, IrLinks, IrLinksVoor;
 
     public Lijnvolger()
     {
+      //eerste is input (witte draad), tweede is output (rode draad)
       IrMidden = new IrLijnvolger(6, 5);
       IrRechts = new IrLijnvolger(4, 3);
       IrLinks = new IrLijnvolger(8, 7);
+      IrLinksVoor = new IrLijnvolger(10,9);
     }
 
 
     public int readSensor()
     {
       int waarde = -1;
-           if(IrMidden.readSensorLijnvolger() && IrRechts.readSensorLijnvolger() && IrLinks.readSensorLijnvolger()){
+      if(IrMidden.readSensorLijnvolger() && IrRechts.readSensorLijnvolger() && IrLinks.readSensorLijnvolger() && IrLinksVoor.readSensorLijnvolger()){
+          waarde = 5;
+          }
+      else if(IrMidden.readSensorLijnvolger() && IrRechts.readSensorLijnvolger() && IrLinks.readSensorLijnvolger()){
             waarde = 4;
-      } else
-           if(IrMidden.readSensorLijnvolger()){
+      } else   if(IrMidden.readSensorLijnvolger()){
            waarde = 0;
            }
 
@@ -32,7 +34,8 @@ public class Lijnvolger
 
       else if(IrLinks.readSensorLijnvolger()){
            waarde = 2;
-           }
+
+      }
       else{
            waarde = 3;
            }
